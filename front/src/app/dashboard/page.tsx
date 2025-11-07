1d93ce0b326b2b8b3d2db6f2eb12db24f8956181
asadullah-alt <asadullahbeg@outlook.com>
2025-11-07 12:32:54 +0500

    removed the question mark

--- front/src/app/dashboard/page.tsx
+++ front/src/app/dashboard/page.tsx
@@ -57,7 +57,7 @@ interface ApiResponse {
 interface TransformedJob {
   id: number;
   jobPosition: string;
-  job_id?: string;
+  job_id: string;
   company: string;
   companyDetails: {
     industry: string | null;
