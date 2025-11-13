8b71f8c3065978600346dc8c71eb1b279645a05d
asadullah-alt <asadullahbeg@outlook.com>
2025-11-13 14:46:31 +0500

    Removed resumes

--- front/src/app/resumes/[id]/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react'
-
-export default function ResumeEditorLayout({ children }: { children: ReactNode }) {
-  return (
-    <div className="flex h-full max-h-screen overflow-hidden">
-      <div className="relative z-10 mx-auto w-full">{children}</div>
-    </div>
-  )
-}
