c4222af0a830aaa83d924f62a7a0a370ca44908a
asadullah-alt <asadullahbeg@outlook.com>
2026-01-02 00:43:17 +0500

    Added signin page

---
+++ front/src/app/signin/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import React from 'react'
+import { BackgroundPathsSignin } from "@/components/ui/background-paths-signin"
+export default function SignInPage() {
+    return (
+
+
+        <BackgroundPathsSignin />
+
+    )
+}
