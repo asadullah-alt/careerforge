a024ae267167af227e47b6bb4dcf61300e7f82b0
asadullah-alt <asadullahbeg@outlook.com>
2026-02-12 21:35:51 +0500

    Modified the smtp for no junk mails

--- back/app/utils/emailService.js
+++ back/app/utils/emailService.js
@@ -7,8 +7,8 @@ const transporter = nodemailer.createTransport({
   // finally in a bloody environment variable
   // thank god
   host: process.env.EMAIL_HOST,
-  port: 465,
-  secure: true, // true for 465, false for other ports
+  port: 2525,
+  secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD
